9e191167426a5808730dae4822935d30f719636e
nmane41 <66508231+nmane41@users.noreply.github.com>
2026-04-18 11:34:51 +0530

    NG-1001 - created project for List model binding (#1)
    * created project for List model binding
    
    * added customer controller

---
+++ Parameterized_indexMethod/Controllers/CustomerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parameterized_indexMethod.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
